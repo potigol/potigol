dd4731d3643fec1b5fba7b49a91f71b4d8087fd6
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2015-02-02 11:28:42 -0300

    Removendo arquivos desnecessários

--- config.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-doskey potigol=@echo off $T java -cp %POTI%/potigol.jar;%POTI%/antlr-runtime-4.1.jar MyPotigol $1 $g $1.scala $Tscala -cp %POTI%/potigol.jar;%POTI%/antlr-runtime-4.1.jar $1.scala $T @echo on
-@echo on
-@echo Potigol configurado!
